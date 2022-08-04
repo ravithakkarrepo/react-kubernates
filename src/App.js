06ae57c3fd84b5ef3fe5ef2493a22f165c91dc62
ravithakkarrepo <ravithakkarweb@gmail.com>
2022-08-04 19:27:40 +0530

    forth

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save.
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       <p>
+          Welcome to Ravi Bhojani Application!
         </p>
         <a
           className="App-link"
